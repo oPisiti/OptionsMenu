e92d140047dcb30965e051aac0042a49edab1f42
oPisiti <78967454+oPisiti@users.noreply.github.com>
2023-05-09 01:25:17 -0300

    Now works on Linux as well
    The usage of the module "msvcrt" limited this module to Windows.
    Now uses "pynput", a more general solution

--- OptionsMenu.py
+++ OptionsMenu.py
@@ -1,8 +1,9 @@
-from msvcrt import getch
-import os
+from pynput import keyboard
+import subprocess
+from time import sleep
 
-# More colors: https://stackoverflow.com/questions/287871/how-to-print-colored-text-to-the-terminal
 class bcolors:
+    """ Font colors """
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKCYAN = '\033[96m'
@@ -14,17 +15,49 @@ class bcolors:
     UNDERLINE = '\033[4m'
     UPONELINE = '\033[F'
 
-# This function loops printing a greeting and the options
-# The current option is printed in green and when chosen (enter key) returns that string
-# While the user chooses an option (arrow keys), everything inside this function is rewritten on top
-# of what is on screen. Nothing other than what is written INSIDE this function gets rewritten
+
+# Keyboard
+KEYS_PRESSED = {
+    "up":    False,
+    "down":  False,
+    "enter": False
+}
+
+def update_keys_pressed(key) -> None:
+    """ Updates the currently pressed keys' dictionary """
+    
+    global KEYS_PRESSED 
+    KEYS_PRESSED = {
+        "up":    key == keyboard.Key.up,
+        "down":  key == keyboard.Key.down,
+        "enter": key == keyboard.Key.enter
+    }
+    pass
+
+# Adding listener for keyboard
+listener = keyboard.Listener(
+    suppress   = True,
+    on_press   = update_keys_pressed)
+listener.start()
+
+
 def OptionsMenu(opt, greeting = "Choose an Option:"):
-    os.system('color')      # This command needs to be called in order for the colors to be changed
+    """
+    This function loops printing a greeting and the options
+    The current option is printed in green and when chosen (enter key) returns that string
+    While the user chooses an option (arrow keys), everything inside this function is rewritten on top
+    of what is on screen. Nothing other than what is written INSIDE this function gets rewritten
+    """
+
+    global KEYS_PRESSED
+
     chosen = False
     choice = 0
 
+
     while True:
 
+        # Printing greeting and options
         print(greeting)
         for i in range(len(opt)):
             if i == choice:    
@@ -32,27 +65,33 @@ def OptionsMenu(opt, greeting = "Choose an Option:"):
 
             else:
                 print(opt[i]) 
-        
-        stroke = ord(getch())
 
-        if stroke == 72:        # Up
-            if choice > 0:
-                choice -= 1 
-            else:
-                choice = len(opt) - 1          
-        elif stroke == 80:      # Down
-            if choice < len(opt) - 1:
-                choice +=1
-            else:
-                choice = 0
-        elif stroke == 13:      # Enter
+        # Handling key presses
+        if KEYS_PRESSED["up"]:                 
+            if choice > 0: choice -= 1 
+            else:          choice = len(opt) - 1
+            
+            KEYS_PRESSED["up"] = False  
+
+        elif KEYS_PRESSED["down"]:
+            if choice < len(opt) - 1: choice +=1
+            else:                     choice = 0
+
+            KEYS_PRESSED["down"] = False  
+
+        elif KEYS_PRESSED["enter"]: 
+            KEYS_PRESSED["enter"] = False
+
+            listener.stop()
             return opt[choice]
+
 
         # Moving to the first line
         for i in range(len(opt) + 1):
             print("", end = bcolors.UPONELINE)  # Returns only the amount of lines used in this function
-        
 
+        # Preventing the screen to flick
+        sleep(0.1)
 
 if __name__ == "__main__":
     print("MY HUMBLE CODE")
